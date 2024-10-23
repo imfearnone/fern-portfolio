d75986b7d672279f56824bb97dc277048d27e945
imfearnone <retardo.fernan@gmail.com>
2024-10-24 02:27:22 +0800

    24/10/2024 227 - Vercel requirements

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-"use client"
-
-import GridBackground from "@/components/GridBackground";
+'use client'
+import React from "react";
 import Projects from "@/components/Projects";
 import About from "@/components/About";
 import Contacts from "@/components/Contacts";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import GridBackground from "@/components/GridBackground";
 
 export default function Page() {
   return (  
