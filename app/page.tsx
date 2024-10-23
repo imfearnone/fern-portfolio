dbc04b15d2b5a3e3b707e7fef846939a8e14b74b
imfearnone <retardo.fernan@gmail.com>
2024-10-24 02:46:42 +0800

    24/10/2024 246 - Vercel requirements

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import GridBackground from "../components/GridBackground";
+import GridBackground from "@/components/GridBackground";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import About from "../components/About";
